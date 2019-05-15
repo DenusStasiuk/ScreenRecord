b5541c65c8c260fcd26284de8b8d42f58dfbbc2b
DenusStasiuk <TitaN28ua>
2019-05-15 14:25:54 +0300

    Add project files.

---
+++ ScreenRecord/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ScreenRecord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Screenshot = new System.Windows.Forms.Button();
+            this.Record = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Screenshot
+            // 
+            this.Screenshot.Location = new System.Drawing.Point(1098, 26);
+            this.Screenshot.Name = "Screenshot";
+            this.Screenshot.Size = new System.Drawing.Size(103, 23);
+            this.Screenshot.TabIndex = 0;
+            this.Screenshot.Text = "ScreenShot";
+            this.Screenshot.UseVisualStyleBackColor = true;
+            this.Screenshot.Click += new System.EventHandler(this.Screenshot_Click);
+            // 
+            // Record
+            // 
+            this.Record.Location = new System.Drawing.Point(1100, 73);
+            this.Record.Name = "Record";
+            this.Record.Size = new System.Drawing.Size(100, 23);
+            this.Record.TabIndex = 1;
+            this.Record.Text = "Record";
+            this.Record.UseVisualStyleBackColor = true;
+            this.Record.Click += new System.EventHandler(this.Record_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1029, 575);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 575);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Record);
+            this.Controls.Add(this.Screenshot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Screenshot;
+        private System.Windows.Forms.Button Record;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ScreenRecord/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenRecord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Screenshot_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(bm);
+            g.CopyFromScreen(0, 0, 0, 0, bm.Size);
+            pictureBox1.Image = bm;
+        }
+
+        void Capture()
+        {
+            while (true)
+            {
+                Bitmap bm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                Graphics g = Graphics.FromImage(bm);
+                g.CopyFromScreen(0, 0, 0, 0, bm.Size);
+                pictureBox1.Image = bm;
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Record_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(Capture);
+            t.Start();
+        }
+    }
+}
